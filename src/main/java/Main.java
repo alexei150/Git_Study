53bdb3f910a104547601553059896e32752de182
alt277 <altima875@gmail.com>
2023-01-15 00:11:11 +0300

    one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,9 @@ public class Main {
         System.out.println("master");
         System.out.println("master");
         System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
+
+        System.out.println("MY");
+        System.out.println("MY");
+
     }
 }
