6574ea6ac0c1ba97b996e78e26a1e9d6288202c4
alt277 <altima875@gmail.com>
2023-02-04 20:27:08 +0300

    master changes 1

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -1,7 +1,6 @@
 public class AnotherClass {
     public static void main(String[] args) {
-        System.out.println("master changes 20:15");
-        System.out.println("master changes 20:17");
-        System.out.println("master changes 20:18");
+        System.out.println("master changes 1 ");
+
     }
 }
