4bb2c7f8fcf73341b8a5cff08a7cb919622d0a52
alt277 <altima875@gmail.com>
2023-02-04 20:36:10 +0300

    master 7000

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -1,6 +1,8 @@
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("master changes 5 ");
-      
+        System.out.println("master changes 6 ");
+        System.out.println("master changes 700 ");
+
     }
 }
