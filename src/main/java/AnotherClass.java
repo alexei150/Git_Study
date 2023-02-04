915e5253f267730e04f70d1cf5d8453518b7c92d
alt277 <altima875@gmail.com>
2023-02-04 20:05:39 +0300

    rkk changes 20:05

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -1,5 +1,6 @@
 public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("Rkk changes 20:00");
+        System.out.println("Rkk changes 20:05");
     }
 }
