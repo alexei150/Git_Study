36a32f2f40f6e48c5715a4014d7035c7ad8833a4
alt277 <altima875@gmail.com>
2023-03-25 19:22:21 +0300

    commited

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -2,6 +2,10 @@ public class AnotherClass {
     public static void main(String[] args) {
         System.out.println("master changes 0 ");
         System.out.println("master changes 1 ");
+        System.out.println("master changes 2 ");
+        System.out.println("master changes 3 ");
+        System.out.println("master changes 4 ");
+        System.out.println("master changes commited ");
 
     }
 }
