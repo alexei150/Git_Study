990bfa7db641587a4b588212c7a67d659813dc36
alt277 <altima875@gmail.com>
2023-03-25 19:58:37 +0300

    heard forward master 4

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -12,6 +12,7 @@ public class AnotherClass {
         System.out.println("master changes head forward master1 ");
         System.out.println("master changes head forward master2 ");
         System.out.println("master changes head forward master3 ");
+        System.out.println("master changes head forward master 4 ");
 
     }
 }
