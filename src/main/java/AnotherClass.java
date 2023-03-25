59c2292ceec97b9065959873d3e50b8cbf5d357a
alt277 <altima875@gmail.com>
2023-03-25 16:01:23 +0300

    000

--- src/main/java/AnotherClass.java
+++ src/main/java/AnotherClass.java
@@ -1,17 +1,7 @@
 public class AnotherClass {
     public static void main(String[] args) {
-        System.out.println("master changes 5 ");
-        System.out.println("master changes 6 ");
-        System.out.println("master changes 700 ");
-        System.out.println("master changes 7000 ");
-        System.out.println("master changes 7000 ");
-        System.out.println("master changes 7000 ");
-        System.out.println("master changes 8000 ");
-        System.out.println("master changes 9000 ");
-        System.out.println("master changes 10000 ");
-        System.out.println("master changes 11000 ");
-        System.out.println("master changes 12000 ");
-
+        System.out.println("master changes 0 ");
+        System.out.println("master changes 1 ");
 
     }
 }
