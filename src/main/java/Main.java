733b071c4f11578b54a6b56a310127c58fdecbaf
alt277 <altima875@gmail.com>
2023-06-05 22:37:42 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("MY 1");
-        System.out.println("MY 2");
-        System.out.println("MY 3");
-        System.out.println("MY 4");
-        System.out.println("MY 5");
+        System.out.println("1");
+
 
     }
 }
