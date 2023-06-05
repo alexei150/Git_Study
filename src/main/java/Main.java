68d6a132fe347bb87b5c4785d56ecd1250e35a37
alt277 <altima875@gmail.com>
2023-06-05 13:16:18 +0300

    master 70

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("master");
-        System.out.println("master");
-        System.out.println("master");
 
-        System.out.println("MY");
-        System.out.println("MY");
-
-
-
-        System.out.println("development");
-        System.out.println("development");
-        System.out.println("development");
-
-        System.out.println("RKK local");
-        System.out.println("RKK changes");
-        System.out.println("RKK changes2");
-
-        System.out.println("aside changes1");
-        System.out.println("aside changes2");
-
+        System.out.println("aside changes5");
+        System.out.println("aside changes5");
         System.out.println("aside changes5");
         System.out.println("aside changes5");
 
