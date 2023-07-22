84e62b72abf604a6737adbfba47967afb7251f4c
alt277 <altima875@gmail.com>
2023-07-22 18:15:06 +0300

    второй

--- src/main/java/AnotherMain.java
+++ src/main/java/AnotherMain.java
@@ -1,23 +1,7 @@
 public class AnotherMain {
     public static void main(String[] args) {
         System.out.println("master changes 0 ");
-        System.out.println("master changes 1 ");
-        System.out.println("master changes 2 ");
-        System.out.println("master changes 3 ");
-        System.out.println("master changes 4 ");
-        System.out.println("master changes commited ");
-        System.out.println("master changes head forward ");
-        System.out.println("master changes head forward 1 ");
-        System.out.println("master changes head forward master ");
-        System.out.println("master changes head forward master1 ");
-        System.out.println("master changes head forward master2 ");
-        System.out.println("master changes head forward master3 ");
-        System.out.println("master changes head forward master 4 ");
-        System.out.println("master changes head forward master 5 ");
-        System.out.println("master changes head forward master 6 ");
-        System.out.println("master changes head forward master 7 ");
-        System.out.println("master changes head forward master 8- ");
-        System.out.println("master changes head forward master 9- ");
+
 
     }
 }
